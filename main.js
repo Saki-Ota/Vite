e53437f42c9dcd669f2ad9298a306f33e285fde9
Saki Ota <saki@SakinoMacBook-Air.local>
2022-11-16 21:02:40 +1100

    added tailwind

--- main.js
+++ main.js
@@ -1,24 +1,106 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const openBtn = document.getElementById("js-open-modal-btn");
+const modal = document.getElementById("js-modal");
+const closeBtn = document.getElementById("js-close-btn");
+const overlay = document.getElementById("js-overlay");
+const ul = document.getElementById("js-ul");
+const openListBtn = document.getElementById("js-open-list-button")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-    <h1> Hello </h1>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const renderLoading = () => {
+  const loading = document.getElementById("js-loading");
+  const loadingImg = document.createElement("img");
+  loadingImg.src = "img/loading.gif";
+  loadingImg.id = "loading-img";
+  loading.appendChild(loadingImg);
+};
+
+const removeLoading = () => {
+  document.getElementById("loading-img").remove();
+};
+
+const renderLists = (features) => {
+  const fragment = new DocumentFragment();
+
+  for (let feature of features) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    a.href = feature.to;
+    a.textContent = feature.text;
+    img.src = feature.img;
+    img.alt = feature.alt;
+
+    fragment
+      .appendChild(li)
+      .appendChild(a)
+      .insertAdjacentElement("afterbegin", img);
+  }
+
+  ul.appendChild(fragment);
+};
+
+const displayInfo = (info) => {
+  const li = document.createElement('li');
+  if (typeof info === 'string') {
+    li.textContent = info;
+    ul.appendChild(li);
+    return console.log(info);
+  }
+  throw new Error('Please pass a string value');
+};
+
+const displayErrorInfo = (error) => {
+  const li = document.createElement('li');
+  li.textContent = `error: ${JSON.stringify(error)}`;
+  ul.appendChild(li);
+};
+
+const getData = async (api) => {
+  let res = null;
+  try {
+    res = await fetch(api);
+  } catch (e) {
+    displayErrorInfo(e);
+  }
+  if (res.ok) {
+    return res.json();
+  } else {
+    displayErrorInfo(res);
+  }
+};
+
+const displayList = async () => {
+  let res = await getData("https://mocki.io/v1/ee8a871e-2b46-4a91-b565-4d6f9216f300");
+  removeLoading();
+
+  if (res.data) {
+    renderLists(res.data);
+  } else {
+    displayInfo("no data");
+  }
+};
+
+const displayModal= () => {
+  modal.style.display= "block";
+  overlay.style.display= "block";
+};
+
+const closeModal = () => {
+  modal.style.display="none";
+  overlay.style.display="none";
+};
+
+const init = () => {
+  openListBtn.remove();
+  modal.remove();
+  overlay.remove();
+  openBtn.remove();
+  renderLoading();
+  displayList();
+};
+
+
+openListBtn.addEventListener("click", init);
+openBtn.addEventListener('click', displayModal);
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
